aac3a4a6e1cdda1ddf44e92251decd4bba549899
Developer-Bear <44989435+Developer-Bear@users.noreply.github.com>
2020-02-03 22:35:55 -0600

    add Spanish language

---
+++ locales/es.lua
@@ -0,0 +1,6 @@
+Locales['en'] = {
+    ['pageFloatingText']   = "Presiona ~g~L~s~ para llamar a un oficial",
+    ['notificationText'] = "Pedido de ayuda en ^5%s^6 en la mesa central",
+    ['successfulPage'] = "¡Buscaste ayuda!",
+    ['recentPage'] = "El departamento fue localizado recientemente. ¡Por favor sea paciente!",
+  }
