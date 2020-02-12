8b44099d6ffd95945b3afe4bf05e9dca63e9638b
kevi2796 <59027271+kevi2796@users.noreply.github.com>
2020-02-12 03:38:36 +0100

    Danish translation

---
+++ locales/dk.lua
@@ -0,0 +1,6 @@
+Locales['dk'] = {
+    ['pageFloatingText']   = "Tryk ~g~L~s~ for at kalde på personalet",
+    ['notificationText'] = "En person har tilkaldt personalet ved ^5%s^7 Receptionen",
+    ['successfulPage'] = "Du har kaldt på personalet!",
+    ['recentPage'] = "Receptionen er allerede blevet kaldt på fornyligt, vent venligst!",
+  }
