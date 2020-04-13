b4dbd32798fba5fe26abdaa35c685dec1d3636d6
Developer-Bear <44989435+Developer-Bear@users.noreply.github.com>
2020-04-12 23:06:30 -0500

    added french language support

---
+++ locales/fr.lua
@@ -0,0 +1,6 @@
+Locales['fr'] = {
+    ['pageFloatingText'] = "Appuyez sur ~g~L~s~ pour demander de l'assistance",
+    ['notificationText'] = "Assistance demandé à la réception du ^5%s^7",
+    ['successfulPage'] = "Vous avez demandé de l'assistance !",
+    ['recentPage'] = "Une demande d'assistance à déjà reçu, veuillez patienter !",
+}
